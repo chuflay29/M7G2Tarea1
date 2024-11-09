98067ce19ffa8b9787298fe220560a33c3dc4db9
jose ona <fernandocarrasc590@gmail.com>
2024-11-09 12:46:16 -0400

    saludoController add

---
+++ M7Tarea1.Server/Controllers/SaludoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace M7Tarea1.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SaludoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetSaludo()
+        {
+            return Ok("Hola, mundo");
+        }
+    }
+}
